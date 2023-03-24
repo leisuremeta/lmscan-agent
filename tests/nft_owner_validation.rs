cda48ab80170143d16b5525ba65663f0466fed15
jichangho <jichangho20@gmail.com>
2023-03-24 15:17:37 +0900

    improve file writer using write buffer

--- tests/nft_owner_validation.rs
+++ tests/nft_owner_validation.rs
@@ -11,7 +11,7 @@ use sea_orm::*;
 
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Write, BufWriter};
 
 static BASE_URI: &str = "http://lmc.leisuremeta.io";
 // static BASE_URI: &str = "http://test.chain.leisuremeta.io";
@@ -40,19 +40,19 @@ async fn validate_nft_owner() {
   // let filename = "/app/playnomm_scan/deploy/test/lmscan-agent/nft_owner_service_202303211130.sql";
   let write_file = "/Users/user/playnomm/source_code/lmscan-agent/output.csv";
 
-  let mut output_file = OpenOptions::new()
+  let mut writer = BufWriter::new(OpenOptions::new()
                                           .append(true)
                                           .open(write_file)
-                                          .expect("cannot open output file");
+                                          .expect("cannot open output file"));
+
   let input_file = File::open(read_file)
                               .expect("cannot open input file");
   let reader = BufReader::new(input_file);
   let mut lines = reader.lines();
   lines.next();
-  for line in lines {
+  for (num, line) in lines.enumerate() {
     let line = line.unwrap();
     let mut items = line.split_whitespace();
-    // println!("{line}");
     let address = items.next().unwrap().trim();
     let token_id = items.next().unwrap().trim();
 
@@ -69,24 +69,27 @@ async fn validate_nft_owner() {
     let current_owner = if nft_opt.is_none() { "NFT 데이터 블록체인에 존재 X".to_string() } else { nft_opt.as_ref().unwrap().current_owner.clone() };
     let is_same_nft_owner = current_owner == address;
     
-    let result = match (is_nft_exist_in_account_from_blc, nft_opt, is_same_nft_owner) {
-      (true,  None,    true)  => 5,
-      (true,  None,    false) => 5,
-      (false, None,    true)  => 5,
-      (false, None,    false) => 5,
-      (true,  Some(_), true)  => 1,
-      (true,  Some(_), false) => 2,
-      (false, Some(_), true)  => 3,
-      (false, Some(_), false) => 4,
+    let result = match nft_opt {
+      Some(_) => match (is_nft_exist_in_account_from_blc, is_same_nft_owner) {
+        (true,  true)  => 1, // Address가 Token 소유하며 Token의 현재 소유주와 일치.
+        (true,  false) => 2, // Address가 Token 소유하나, 현재 토큰의 소유주는 다른 계정.
+        (false, true)  => 3, // Address가 Token 소유하지 않으나, 현재 토큰의 소유주로 되있음.
+        (false, false) => 4, // Address가 Token 소유하지 않으며, 현재 토큰의 소유주도 다른 사람.
+      }
+      None => 5, // NFT 데이터가 블록체인에 존재하지 않음.
     };       
     let token_ids = nft_balance_info.into_keys().collect::<Vec<String>>().join(",");
-    let output = format!("{address}\t{token_id}\t{result}\t{token_ids}\n");
+    let output = format!("'{address}'\t'{token_id}'\t'{result}'\t'{token_ids}'\n");
     println!("{output}");
 
-    output_file
-        .write(output.as_bytes())
-        .expect("write failed");
+    writeln!(writer, "This is a new line.").unwrap();
+    if num % 100 == 1 {
+      writer.flush().unwrap();
+    }
+    // output_file
+    //     .write(output.as_bytes())
+    //     .expect("write failed");
   }
-
+  writer.flush().unwrap();
 
 }
