a8de79924baeaba7539c409527f16ed99158687a
jichangho <jichangho20@gmail.com>
2023-06-02 11:28:08 +0900

    add nft_file_info_update test

--- tests/nft_file_info_update.rs
+++ tests/nft_file_info_update.rs
@@ -28,26 +28,33 @@ async fn nft_file_info_update() {
       if let TokenTx::MintNft(mint_tx) = tx {
         let nft_meta_info_opt: Option<NftMetaInfo> = ApiService::get_request_until(mint_tx.data_url.clone(), 6).await;
         let nft_file_opt = nft_file_map.get_mut(&mint_tx.token_id);
-        match (nft_meta_info_opt, nft_file_opt) {
-          (Some(info), Some(nft_file)) =>  {
-            nft_file.collection_name = info.collection_name;
-            nft_file.creator = info.creator;
-            nft_file.creator_description = info.collection_description;
-            nft_file.data_url = mint_tx.data_url;
-            nft_file.rarity = info.rarity;
-            nft_file.event_time = as_timestamp(&mint_tx.created_at);
-            nft_file.owner = mint_tx.output;
-            nft_file.nft_uri = info.nft_uri;
-            nft_file.nft_name = info.nft_name;
 
-            let nft_file_model = nft_file.clone().into_active_model();
+        let nft_file = nft_file::Model::from(&mint_tx, nft_meta_info_opt);
+        nft_file::Entity::update(nft_file).exec(db).await.unwrap();
 
-            nft_file::Entity::update(nft_file_model).exec(db).await.unwrap();
-          },
-          _ => {
-            println!("mint_tx error: {:?}", mint_tx);
-          }
-        }
+        // match (nft_meta_info_opt, nft_file_opt) {
+        //   (Some(info), Some(nft_file)) =>  {
+        //     nft_file.collection_name = info.collection_name;
+        //     nft_file.creator = info.creator;
+        //     nft_file.creator_description = info.collection_description;
+        //     nft_file.data_url = mint_tx.data_url;
+        //     nft_file.rarity = info.rarity;
+        //     nft_file.event_time = as_timestamp(&mint_tx.created_at);
+        //     nft_file.owner = mint_tx.output;
+        //     nft_file.nft_uri = info.nft_uri;
+        //     nft_file.nft_name = info.nft_name;
+        //     let nft_file = nft_file::Model::from(mint_tx, nft_meta_info_opt);
+
+
+        //     let nft_file_model = nft_file.clone().into_active_model();
+
+        //     println!("update proc : {:?}", nft_file_model);
+        //     nft_file::Entity::update(nft_file_model).exec(db).await.unwrap();
+        //   },
+        //   _ => {
+        //     println!("mint_tx error: {:?}", mint_tx);
+        //   }
+        // }
       }
     }
   };
