cf7c095b1b26698521cd462f9981893524adbe81
jichangho <jichangho20@gmail.com>
2023-06-02 18:14:03 +0900

    add get_balace_from_blockchain by get_request_unti

--- tests/account_balance_test.rs
+++ tests/account_balance_test.rs
@@ -1,10 +1,9 @@
-use std::{fs::File, path::Path, io::Write};
+use std::{fs::File, path::Path, io::Write, collections::HashMap};
 
-use lmscan_agent::{entity::account_entity, service::api_service::ApiService};
+use lmscan_agent::{entity::account_entity, service::api_service::ApiService, model::balance_info::BalanceInfo};
 use dotenvy::{dotenv, var};
 use lmscan_agent::library::common::db_connn;
 use sea_orm::*;
-
 
 
 #[tokio::test]
@@ -26,22 +25,18 @@ async fn account_balance_test() {
                                     .expect("cannot open output file");
   output_file.write(format!("address, blc_balance, scan_balance, equal, diff\n").as_bytes()).expect("write failed");
   for (count, scan_account) in scan_accounts.enumerate() {
-    println!("scan_account: {:?}", scan_account.address);
-    match ApiService::get_account_balance(&scan_account.address).await {
-      Ok(block_account_balance_opt) => {
-        if block_account_balance_opt.is_none() {
-          println!("balance doesn't exist '{}'", scan_account.address);
-          continue;
-        }
+    let address = scan_account.address.clone();
+    println!("scan_account: {:?}", address);
+    // match ApiService::get_account_balance(&scan_account.address).await {
+    match ApiService::get_request_until::<String, Option<HashMap<String, BalanceInfo>>>(format!("http://lmc.leisuremeta.io/balance/{address}?movable=free") , 100).await {
+      Some(block_account_balance_opt) => {
+        // if block_account_balance_opt.is_none() {
+        //   println!("balance doesn't exist '{}'", scan_account.address);
+        //   continue;
+        // }
 
         let block_account_balance = block_account_balance_opt.unwrap();
-        println!("block_account_balance: {:?}", block_account_balance);
-        // block_account_balance.iter().for_each(|(k, v)| println!("{k} - {:?}", v));
-        // println!("block_account_balance: {block_account_balance}");
         if let Some(block_info) = block_account_balance.get("LM") {
-          // assert_eq!() 매크로는 둘이 같은 지를 체크하고 같지 않으면 에러를 냄.
-          // assert_eq!(block_info.total_amount, scan_account.balance);          
-          // let blc_tot_amount = BigDecimal::from_str(&block_info.total_amount).unwrap();
           let line = format!("{}, {}, {}, {}, {}\n", 
                     scan_account.address, 
                     block_info.total_amount,
@@ -50,10 +45,9 @@ async fn account_balance_test() {
                     if block_info.total_amount.clone() > scan_account.balance { block_info.total_amount.clone() - scan_account.balance } else { scan_account.balance - block_info.total_amount.clone() }
                   );
           output_file.write(line.as_bytes()).expect("write failed");
-          println!("{line}");
         }
       }
-      Err(err) => println!("request err: {err}")
+      None => println!("balance doesn't exist '{}'", scan_account.address)
     }
   }
 }
